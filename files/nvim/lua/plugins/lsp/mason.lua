1b87734916fb02e11da0899aced4d9a4ed6a25e9
Caulifwr <62228420+cauliflwr@users.noreply.github.com>
2024-07-28 16:17:57 -0400

    Update Mason UI icons

--- files/nvim/lua/plugins/lsp/mason.lua
+++ files/nvim/lua/plugins/lsp/mason.lua
@@ -1,27 +1,32 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        -- import mason
+        local mason = require("mason")
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
+        -- import mason-lspconfig
+        local mason_lspconfig = require("mason-lspconfig")
 
-		-- enable mason
-		mason.setup({
-			ui = {
-				border = "rounded",
-			},
-		})
+        -- enable mason
+        mason.setup({
+            ui = {
+                border = "rounded",
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                },
+            },
+        })
 
-		mason_lspconfig.setup({
-			ensure_installed = { "bashls", "html", "lua_ls", "pyright", "ruff", "tflint" },
+        mason_lspconfig.setup({
+            ensure_installed = { "bashls", "html", "lua_ls", "pyright", "ruff", "tflint" },
 
-			--enable auto install of lsp configured servers
-			automatic_installation = true,
-		})
-	end,
+            --enable auto install of lsp configured servers
+            automatic_installation = true,
+        })
+    end,
 }
