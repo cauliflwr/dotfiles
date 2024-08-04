8cd1f60fe7399368d3a4009dd9233562e9f507df
Caulifwr <62228420+cauliflwr@users.noreply.github.com>
2024-08-04 13:45:09 -0400

    Add bind to clear search highlights

--- files/nvim/lua/keymaps.lua
+++ files/nvim/lua/keymaps.lua
@@ -14,16 +14,17 @@ vim.keymap.set("n", "<leader>sx", "<cmd>close<cr>", { desc = "close the current 
 
 -- Modes and settings
 vim.keymap.set("i", "<C-e>", "<esc>", { desc = "use c-e to exit insert mode" })
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- [[ Basic Autocommands ]]
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 -- vim: ts=2 sts=2 sw=2 et
